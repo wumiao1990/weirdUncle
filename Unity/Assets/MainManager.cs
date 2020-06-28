912c968cf9f729d0b0a1c60710e21e89f7149c92
wumiao <wumiao@xiimoon.com>
2020-06-28 11:42:52 +0800

    视频播放

--- Unity/Assets/MainManager.cs
+++ Unity/Assets/MainManager.cs
@@ -3,20 +3,78 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class MainManager : MonoBehaviour
 {
+	public RectTransform canvasRT;
 	public Button btn;
+	public Button btn1;
+	public Button btn2;
+	public Button btn3;
+	public RawImage rawImage;
+	public VideoPlayer vPlayer;
 	// Use this for initialization
 	void Start () {
 		btn.onClick.AddListener(() =>
 		{
 			UnityEngine.SceneManagement.SceneManager.LoadScene("HonJin");//直接加载，销毁掉原来的场景
 		});
+		
+		btn1.onClick.AddListener(() =>
+		{
+			PlayVideo("1");
+			//Handheld.PlayFullScreenMovie("1.mp4", Color.white, FullScreenMovieControlMode.Full);
+		});
+		btn2.onClick.AddListener(() =>
+        {
+	        PlayVideo("2");
+	        //Handheld.PlayFullScreenMovie("2.mp4", Color.white, FullScreenMovieControlMode.Full);
+        });
+		btn3.onClick.AddListener(() =>
+		{
+			PlayVideo("3");
+			//Handheld.PlayFullScreenMovie("3.mp4", Color.white, FullScreenMovieControlMode.Full);
+		});
+		
+		ScaleRawImage();
+	}
+
+	void PlayVideo(string name)
+	{
+		VideoClip clip = Resources.Load<VideoClip>(name);
+		rawImage.Active();
+		vPlayer.clip = clip;
+		RenderTexture renderTexture = Resources.Load<RenderTexture>("videoTexture");
+		if (renderTexture != null)
+		{
+			vPlayer.targetTexture = renderTexture;
+			rawImage.texture = renderTexture;
+		}
+		vPlayer.loopPointReached += EndReached;
+		vPlayer.Play();
+	}
+
+	void EndReached(VideoPlayer vPlayer)
+	{
+		rawImage.SetActive(false);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void ScaleRawImage()
+	{
+//		Vector2 spriteSize = new Vector2();
+//		Vector2 imgSize = new Vector2(rawImage.texture.width, rawImage.texture.height);//图片的实现大小
+//		Vector2 canvasSize = canvasRT.sizeDelta;    // 画布的大小，代码手机屏幕的大小
+//		if (imgSize.y / imgSize.x > canvasSize.y / canvasSize.x)
+//		{
+//			spriteSize.x = canvasSize.x;
+//			spriteSize.y = imgSize.y * (spriteSize.x / imgSize.x);
+//		}
+//		else
+//		{
+//			spriteSize.y = canvasSize.y;
+//			spriteSize.x = imgSize.x * (spriteSize.y / imgSize.y);
+//		}
+		rawImage.rectTransform.sizeDelta = new Vector2(Screen.width, Screen.height);
 	}
 }
